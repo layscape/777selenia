a6b3766b4d6f1791e3666c6bd2b077dcf213f6f6
layscape <57191240+layscape@users.noreply.github.com>
2020-08-13 18:48:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ tradeset = {
 	"BaseTrade":"0.00001",
 	"C1":"0", 							#5-95
 	"C2":"0", 							#5-95
-	"NumberOfTrade":"0", 				#max limit 200
+	"NumberOfTrade":"0", 				#Max limit 200, How much you want trading in one time
 	"ResetOnWin":"0", 					# 0=OFF 1=ON, If ON Change "MultiplyOnWin" to 0
 	"ResetOnLose":"0", 					# 0=OFF 1=ON, If ON Change "MultiplyOnLose" to 0
 	"MultiplyOnWin":"0", 				## 0 to OFF || 0.5 = 50%, 1.0 = 100% (double), 2.0 = 200% (triple)
@@ -16,7 +16,7 @@ tradeset = {
 	"ResetOnLoseMaxTrade":"0", 			# 0 to OFF 
 	"StopOnLoseMaxTrade":"0",			# 0 to OFF
 	"StopIfBalance":"0", 				# 0 to OFF
-	"StopMinBalance":"0", 				# *blank* to OFF
+	"StopMinBalance":"", 				# *blank* to OFF
 	"ClientSeed":"999999",				#max 32 digits
 	"RecoveryMultiplier":"0",			# 1=Default. Must More than 0. Multiply your BaseTrade If Lose
 	"RecoveryIncrease":"0"	 			# 0 to OFF. Increase your BaseTrade If Lose
