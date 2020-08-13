727be84285385bb4b1ac8fce531cbbbfa944a8f8
layscape <57191240+layscape@users.noreply.github.com>
2020-08-13 18:33:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 account = {
-	"Username":"kumaru1",
-	"Password":"aqmar2012"
+	"Username":"xxx",
+	"Password":"xxx"
 }
-license = "STB-0FBGA-C4ANL-H8F8F-DVC"
+license = "xxx"
 tradeset = {
 	"BaseTrade":"0.00001",
-	"C1":"50", 							#5-95
-	"C2":"65", 							#5-95
-	"NumberOfTrade":"5", 				#max limit 200
-	"ResetOnWin":"1", 					# 0=OFF 1=ON, If ON Change "IncreaseOnWinPercent" to 0
-	"ResetOnLose":"0", 					# 0=OFF 1=ON, If ON Change "IncreaseOnLosePercent" to 0
+	"C1":"0", 							#5-95
+	"C2":"0", 							#5-95
+	"NumberOfTrade":"0", 				#max limit 200
+	"ResetOnWin":"0", 					# 0=OFF 1=ON, If ON Change "MultiplyOnWin" to 0
+	"ResetOnLose":"0", 					# 0=OFF 1=ON, If ON Change "MultiplyOnLose" to 0
 	"MultiplyOnWin":"0", 				## 0 to OFF || 0.5 = 50%, 1.0 = 100% (double), 2.0 = 200% (triple)
-	"MultiplyOnLose":"2.0", 			## 0 to OFF || 0.5 = 50%, 1.0 = 100% (double), 2.0 = 200% (triple)
+	"MultiplyOnLose":"0", 			## 0 to OFF || 0.5 = 50%, 1.0 = 100% (double), 2.0 = 200% (triple)
 	"MaxBaseTrade":"0",					# 0 to OFF
 	"ResetOnLoseMaxTrade":"0", 			# 0 to OFF 
 	"StopOnLoseMaxTrade":"0",			# 0 to OFF
 	"StopIfBalance":"0", 				# 0 to OFF
 	"StopMinBalance":"0", 				# *blank* to OFF
-	"ClientSeed":"412312",				#max 32 digits
-	"RecoveryMultiplier":"1.5",			# 1=Default. Must More than 0. Multiply your BaseTrade If Lose
+	"ClientSeed":"999999",				#max 32 digits
+	"RecoveryMultiplier":"0",			# 1=Default. Must More than 0. Multiply your BaseTrade If Lose
 	"RecoveryIncrease":"0"	 			# 0 to OFF. Increase your BaseTrade If Lose
 }
 
