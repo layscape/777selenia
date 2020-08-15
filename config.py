401803e082e5d456f198240bd93d46c5061c4824
layscape <57191240+layscape@users.noreply.github.com>
2020-08-15 11:00:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ account = {
 license = "xxx"
 tradeset = {
 	"BaseTrade":"0.00001",
-	"C1":"55", 							#5-95
-	"C2":"65", 							#5-95
-	"NumberOfTrade":"0", 				#max limit 200 					#ON/OFF
-	"MultiplyOnWin":"0", 				## 0 untuk OFF || 0.5 = 50%, 1.0 = 100% (double), 2.0 = 200% (triple)
-	"MultiplyOnLose":"0", 				## 0 untuk OFF || 0.5 = 50%, 1.0 = 100% (double), 2.0 = 200% (triple)
-	"MaxBaseTrade":"0",					# 0 untuk OFF
-	"ResetOnLoseMaxTrade":"OFF", 		# ON/OFF
+	"C1":"55", 					#5-95
+	"C2":"65", 					#5-95
+	"NumberOfTrade":"25", 				#max limit 200
+	"MultiplyOnWin":"0", 				## 0 untuk OFF 
+	"MultiplyOnLose":"3", 				## 0 untuk OFF 
+	"MaxBaseTrade":"0",				# 0 untuk OFF
+	"ResetOnLoseMaxTrade":"OFF", 			# ON/OFF
 	"StopOnLoseMaxTrade":"OFF",			# ON/OFF
-	"TargetProfit":"0",				#0 untuk OFF
-	"ClientSeed":"0",				#max 32 digits
-	"RecoveryMultiplier":"0",			# 1 untuk OFF
+	"TargetProfit":"10",				#0 untuk OFF
+	"ClientSeed":"999999",				#max 32 digits
+	"RecoveryMultiplier":"20",			# 1 untuk OFF
 	"RecoveryIncrease":"0"	 			# 0 untuk OFF. Increase your BaseTrade If Lose
 }
 
