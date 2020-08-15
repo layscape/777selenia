74512be2836d79e635692b1bd7c76a2a777d4b7c
layscape <57191240+layscape@users.noreply.github.com>
2020-08-15 10:56:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,21 +5,18 @@ account = {
 license = "xxx"
 tradeset = {
 	"BaseTrade":"0.00001",
-	"C1":"0", 							#5-95
-	"C2":"0", 							#5-95
-	"NumberOfTrade":"0", 				#Max limit 200, How much you want trading in one time
-	"ResetOnWin":"0", 					# 0=OFF 1=ON, If ON Change "MultiplyOnWin" to 0
-	"ResetOnLose":"0", 					# 0=OFF 1=ON, If ON Change "MultiplyOnLose" to 0
-	"MultiplyOnWin":"0", 				## 0 to OFF || 0.5 = 50%, 1.0 = 100% (double), 2.0 = 200% (triple)
-	"MultiplyOnLose":"0", 			## 0 to OFF || 0.5 = 50%, 1.0 = 100% (double), 2.0 = 200% (triple)
-	"MaxBaseTrade":"0",					# 0 to OFF
-	"ResetOnLoseMaxTrade":"0", 			# 0 to OFF 
-	"StopOnLoseMaxTrade":"0",			# 0 to OFF
-	"StopIfBalance":"0", 				# 0 to OFF
-	"StopMinBalance":"", 				# *blank* to OFF
-	"ClientSeed":"999999",				#max 32 digits
-	"RecoveryMultiplier":"0",			# 1=Default. Must More than 0. Multiply your BaseTrade If Lose
-	"RecoveryIncrease":"0"	 			# 0 to OFF. Increase your BaseTrade If Lose
+	"C1":"55", 							#5-95
+	"C2":"65", 							#5-95
+	"NumberOfTrade":"0", 				#max limit 200 					#ON/OFF
+	"MultiplyOnWin":"0", 				## 0 untuk OFF || 0.5 = 50%, 1.0 = 100% (double), 2.0 = 200% (triple)
+	"MultiplyOnLose":"0", 				## 0 untuk OFF || 0.5 = 50%, 1.0 = 100% (double), 2.0 = 200% (triple)
+	"MaxBaseTrade":"0",					# 0 untuk OFF
+	"ResetOnLoseMaxTrade":"OFF", 		# ON/OFF
+	"StopOnLoseMaxTrade":"OFF",			# ON/OFF
+	"TargetProfit":"0",				#0 untuk OFF
+	"ClientSeed":"0",				#max 32 digits
+	"RecoveryMultiplier":"0",			# 1 untuk OFF
+	"RecoveryIncrease":"0"	 			# 0 untuk OFF. Increase your BaseTrade If Lose
 }
 
 tools = {
