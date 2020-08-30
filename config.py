cde0d9625d0297dd8e3bdd246e5b273cd6ab463c
layscape <57191240+layscape@users.noreply.github.com>
2020-08-30 20:50:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,23 +4,30 @@ account = {
 }
 license = "xxx"
 tradeset = {
-	"BaseTrade":"0.00001",
-	"C1":"55", 					#5-95
-	"C2":"65", 					#5-95
-	"NumberOfTrade":"0", 				#max limit 200
+	"BaseTrade":"0.0001",
+	"C1":"25", 					#5-95
+	"C2":"35", 					#5-95
+	"TradeCount":"35", 				#max limit 200
 	"MultiplyOnWin":"0", 				## 0 untuk OFF 
-	"MultiplyOnLose":"0", 				## 0 untuk OFF 
-	"MaxBaseTrade":"0",				# 0 untuk OFF
-	"ResetOnLoseMaxTrade":"OFF", 			# ON/OFF
-	"StopOnLoseMaxTrade":"OFF",			# ON/OFF
-	"TargetProfit":"0",				#0 untuk OFF
-	"ClientSeed":"0",				#max 32 digits
-	"RecoveryMultiplier":"0",			# 1 untuk OFF
-	"RecoveryIncrease":"0"	 			# 0 untuk OFF. Increase your BaseTrade If Lose
+	"MultiplyOnLose":"0.5", 			## 0 untuk OFF 
+	"MaxBaseTrade":{
+		"Toogle":"OFF",				#ON/OFF
+		"Max":"0.001",
+		"ResetOnLoseMaxTrade":"OFF", 		# ON/OFF
+		"StopOnLoseMaxTrade":"OFF"},		# ON/OFF
+	"ClientSeed":"999999",				#max 32 digits
 }
 
 tools = {
 	"AddDelayTrade":"0",				#Delay Per Trade
 	"AddDelayTradeWin":"0",				#Delay Per Trade Win
-	"AddDelayTradeLose":"0"				#Delay Per Trade Lose
+	"AddDelayTradeLose":"0",			#Delay Per Trade Lose
+	"RecoveryMultiplier":"1.5",			# 1 untuk OFF. BaseTrade Lose Terakhir di Kali RecoveryMultiplier
+	"RecoveryIncrease":"0",	 			# 0 untuk OFF. BaseTrade Lose Terakhir di Tambah RecoveryIncrease
+	"TargetProfit":"20",				#0 untuk OFF
+	"StopLoseBalance":"20", 			#0 untuk OFF
+	"ForceTC1AfterLose":"ON",			#ON/OFF
+	"ChangeTCAfterLose":{
+		"Toogle":"ON",
+		"ChangeTo":"5"} 			
 }
